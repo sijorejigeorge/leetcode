1074aeb3cf0e5d5f1096644f669fb632f0ab1071
sijorejigeorge <148432068+sijorejigeorge@users.noreply.github.com>
2025-05-27 17:11:30 -0500

    Time: 2696 ms (6.54%) | Memory: 13.4 MB (30.56%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -6,8 +6,7 @@ class Solution(object):
         """
         #swicth = False
         #t = 1
-        l = len(nums)
-        for i in range(0, l):
+        #for i in range(0, l):
             #if 0 not in nums and len(nums) > 1:
             #    if t not in nums and t == i:
             #        t = i+1
@@ -15,6 +14,9 @@ class Solution(object):
             #    return 0
             #elif i not in nums:
             #    return i   
-            if i not in nums:
-                return i 
+        #return i+1
+        
+        for i in range(len(nums)):
+           if i not in nums:
+            return i   
         return i+1
