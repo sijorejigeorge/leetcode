15fbbb060a99f0f751ad81184b1d582fc1097d83
sijorejigeorge <148432068+sijorejigeorge@users.noreply.github.com>
2025-05-27 17:01:29 -0500

    Time: 2612 ms (14.13%) | Memory: 13.3 MB (62.26%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -7,11 +7,13 @@ class Solution(object):
         swicth = False
         t = 1
         for i in range(0, len(nums)):
-            if 0 not in nums and len(nums) > 1:
-                if t not in nums and t == i:
-                    t = i+1
-                    continue
-                return 0
-            elif i not in nums:
-                return i    
+            #if 0 not in nums and len(nums) > 1:
+            #    if t not in nums and t == i:
+            #        t = i+1
+            #        continue
+            #    return 0
+            #elif i not in nums:
+            #    return i   
+            if i not in nums:
+                return i 
         return i+1
