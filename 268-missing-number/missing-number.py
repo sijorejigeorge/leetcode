778c9590cb0d89c8c94a44263e746b461390010a
sijorejigeorge <148432068+sijorejigeorge@users.noreply.github.com>
2025-05-27 16:59:37 -0500

    Time: 4740 ms (5.02%) | Memory: 13.1 MB (99.73%) - LeetSync

---
+++ 268-missing-number/missing-number.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        swicth = False
+        t = 1
+        for i in range(0, len(nums)):
+            if 0 not in nums and len(nums) > 1:
+                if t not in nums and t == i:
+                    t = i+1
+                    continue
+                return 0
+            elif i not in nums:
+                return i    
+        return i+1
